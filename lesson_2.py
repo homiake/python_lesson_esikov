5dc8d7438526b9abb4de1e56ca8e78b20e25c301
Vibris <homiake@yandex.ru>
2022-09-05 18:42:42 +0300

    finished lesson 2

--- lesson_2.py
+++ lesson_2.py
@@ -1,20 +1,20 @@
 # Exercise 1
-# def isfloat(y):
-#     try:
-#         float(y)
-#     except ValueError:
-#         return False
-#     return True
-#
-#
-# n = 0
-# while n < 3:
-#     x = input('Введите вещественное число = ')
-#     if not isfloat(x):
-#         print('Введено не вещественное число')
-#     else:
-#         print(f'Сумма цифр числа {sum(map(int, str(abs(float(x))).replace(".", "")))}')
-#     n += 1
+def isfloat(y):
+    try:
+        float(y)
+    except ValueError:
+        return False
+    return True
+
+
+n = 0
+while n < 3:
+    x = input('Введите вещественное число = ')
+    if not isfloat(x):
+        print('Введено не вещественное число')
+    else:
+        print(f'Сумма цифр числа {sum(map(int, str(abs(float(x))).replace(".", "")))}')
+    n += 1
 
 # Exercise 2
 def isint(y):
@@ -23,21 +23,21 @@ def isint(y):
     except ValueError:
         return False
     return True
-#
-#
-# n = 0
-# count = 1
-# arr = []
-# while n < 3:
-#     x = input('Введите целое число = ')
-#     if not isint(x):
-#         print('Введено не целое число')
-#     else:
-#         for i in range(1, int(x)+1):
-#             count *= i
-#             arr.append(count)
-#         print(f'Вектор значений {arr}')
-#     n += 1
+
+
+n = 0
+count = 1
+arr = []
+while n < 3:
+    x = input('Введите целое число = ')
+    if not isint(x):
+        print('Введено не целое число')
+    else:
+        for i in range(1, int(x)+1):
+            count *= i
+            arr.append(count)
+        print(f'Вектор значений {arr}')
+    n += 1
 
 # Exercise 3
 n = 0
